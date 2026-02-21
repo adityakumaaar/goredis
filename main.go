0977e3e66e687c9c24baf3156497db7273a30271
Aditya Kumar <135761875+adityakumaaar@users.noreply.github.com>
2026-02-21 14:02:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,46 @@ import (
 	"strings"
 )
 
+func handleConnection(conn net.Conn, aof *Aof) {
+	defer conn.Close()
+
+	resp := NewResp(conn)
+	writer := NewWriter(conn)
+
+	for {
+		value, err := resp.Read()
+		if err != nil {
+			return
+		}
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
+		}
+
+		result := handler(args)
+		writer.Write(result)
+	}
+}
+
 func main() {
 	fmt.Println("Listening on port :6379")
 
@@ -16,7 +56,7 @@ func main() {
 		return
 	}
 
-	aof, err := NewAof("/Users/aditya/repos/goredis/database.aof")
+	aof, err := NewAof("database.aof")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -36,49 +76,13 @@ func main() {
 		handler(args)
 	})
 
-	// Listen for connections
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
-
 	for {
-		resp := NewResp(conn)
-		value, err := resp.Read()
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
-			return
-		}
-		if value.typ != "array" {
-			fmt.Println("Invalid request, expected array")
 			continue
 		}
 
-		if len(value.array) == 0 {
-			fmt.Println("Invalid request, expected array length > 0")
-			continue
-		}
-
-		command := strings.ToUpper(value.array[0].bulk)
-		args := value.array[1:]
-
-		writer := NewWriter(conn)
-
-		handler, ok := Handlers[command]
-
-		if !ok {
-			fmt.Println("Invalid command: ", command)
-			writer.Write(Value{typ: "string", str: ""})
-			continue
-		}
-
-		if command == "SET" || command == "HSET" {
-			aof.Write(value)
-		}
-
-		result := handler(args)
-		writer.Write(result)
+		go handleConnection(conn, aof)
 	}
 }
